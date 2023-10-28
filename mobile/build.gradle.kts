5a476b1074724b94bc6f1400b1144e1dee36ca0a
Yazan Tarifi <yazantarifi989@gmail.com>
2023-10-28 03:39:38 +0300

    Fix Android Module Configurations

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -38,6 +38,10 @@ android {
         jvmTarget = "17"
     }
 
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
     buildFeatures {
         compose = true
         buildConfig = true
@@ -46,13 +50,16 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:31.4.0"))
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
+
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("com.google.firebase:firebase-crashlytics-ktx")
     implementation("com.google.firebase:firebase-analytics-ktx")
--- mobile/src/main/java/com/yazantarifi/mobile/MainActivity.kt
+++ mobile/src/main/java/com/yazantarifi/mobile/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.yazantarifi.mobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+
+        }
     }
 }
